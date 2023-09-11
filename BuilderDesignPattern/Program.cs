2d2f10111e9b6ad3a84009c15216daf8c19a930a
didem demircan <didem88.dd@gmail.com>
2023-09-11 19:36:11 +0300

    last v

--- BuilderDesignPattern/Program.cs
+++ BuilderDesignPattern/Program.cs
@@ -8,89 +8,208 @@ Console.WriteLine("Hello, World!");
 //mercedes.Model = "MMOLDE-X";
 //mercedes.Vites = true;
 
-interface IArabaBuilder
-{
-    public Araba araba { get; }
+//ArabaDirector arabaDirector = new ArabaDirector();
+//Araba mercedes =arabaDirector.Build(new MercedesBuilder());
+//mercedes.ToString();
 
-    void setMarka();
-    void setModel();
+//Araba opel= arabaDirector.Build(new OpelBuilder());
+//opel.ToString();
+#region abstract ile tasaralnması 
 
-    void setKM();
+//abstract class ArabaBuilder
+//{
 
-    void setVites();
+//    protected Araba araba;
 
+//    public Araba Araba { get => araba; }
 
+//    public ArabaBuilder()
+//    {
+//        araba = new Araba();
+//    }
 
+//    public abstract ArabaBuilder  setMarka();
+//    public abstract ArabaBuilder setModel();
 
-}
+//    public abstract ArabaBuilder setKM();
 
-class MercedesBuilder : IArabaBuilder
-{
-    public Araba araba { get; }
-
-    public MercedesBuilder()
-    {
-        araba = new();
-
-    }
-
-    public void setKM() => araba.KM = 1;
-
-
-    public void setMarka() => araba.Marka = "Mercedes";
+//    public abstract ArabaBuilder setVites();
 
 
 
-    public void setModel() => araba.Model = "MMOLDE-X";
+
+//}
 
 
-    public void setVites() => araba.Vites = true;
+//class OpelBuilder : ArabaBuilder
+//{
    
-}
-
-class OpelBuilder : IArabaBuilder
-{
-    public Araba araba { get; }
-
-    public OpelBuilder()
-    {
-        araba = new();
-
-    }
-
-    public void setKM() => araba.KM = 1;
 
 
-    public void setMarka() => araba.Marka = "OPEL";
+//    public override ArabaBuilder setKM() 
+//    {
+//        araba.KM = 1; return this;
+
+    
+//    }
+
+
+//    public override ArabaBuilder setMarka()
+//    {
+//        araba.Marka = "oPEL"; return this;
+
+
+//    }
 
 
 
-    public void setModel() => araba.Model = "MMOLDE-y";
+//    public override ArabaBuilder setModel()
+//    {
+//        araba.Model = "MODEL-RR"; return this;
 
 
-    public void setVites() => araba.Vites = true;
-
-}
+//    }
 
 
-class Araba
-{ 
-
-    public string Marka { get; set; }
-
-    public string Model { get;set; }
-
-    public double KM { get; set; }  
-
-    public bool Vites { get; set; }
+//    public override ArabaBuilder setVites()
+//    {
+//        araba.Vites = true; return this;
 
 
-    public override string ToString()
-    {
-        Console.WriteLine($"{Marka} marka araba {Model} modelinde {KM} kilometrede {Vites} vitesolarak üretilmiştir.");
+//    }
 
-        return base.ToString(); 
-    }
+//}
+
+//class Araba
+//{
+
+//    public string Marka { get; set; }
+
+//    public string Model { get; set; }
+
+//    public double KM { get; set; }
+
+//    public bool Vites { get; set; }
 
 
-}
+//    public override string ToString()
+//    {
+//        Console.WriteLine($"{Marka} marka araba {Model} modelinde {KM} kilometrede {Vites} vitesolarak üretilmiştir.");
+
+//        return base.ToString();
+//    }
+
+
+//}
+
+
+//#endregion
+
+
+
+#region inreface ile tasaralanması 
+//interface IArabaBuilder
+//{
+//    public Araba araba { get; }
+
+//    void setMarka();
+//    void setModel();
+
+//    void setKM();
+
+//    void setVites();
+
+
+
+
+//}
+
+////direcotor
+
+//class ArabaDirector
+//{
+//    public Araba Build(IArabaBuilder arabaBuilder)
+//    {
+//        arabaBuilder.setMarka();
+//        arabaBuilder.setModel();
+//        arabaBuilder.setKM();
+//        arabaBuilder.setVites();
+//        return arabaBuilder.araba;
+
+
+
+//    }
+
+//}
+
+//class MercedesBuilder : IArabaBuilder
+//{
+//    public Araba araba { get; }
+
+//    public MercedesBuilder()
+//    {
+//        araba = new();
+
+//    }
+
+//    public void setKM() => araba.KM = 1;
+
+
+//    public void setMarka() => araba.Marka = "Mercedes";
+
+
+
+//    public void setModel() => araba.Model = "MMOLDE-X";
+
+
+//    public void setVites() => araba.Vites = true;
+
+//}
+
+//class OpelBuilder : IArabaBuilder
+//{
+//    public Araba araba { get; }
+
+//    public OpelBuilder()
+//    {
+//        araba = new();
+
+//    }
+
+//    public void setKM() => araba.KM = 1;
+
+
+//    public void setMarka() => araba.Marka = "OPEL";
+
+
+
+//    public void setModel() => araba.Model = "MMOLDE-y";
+
+
+//    public void setVites() => araba.Vites = true;
+
+//}
+
+
+//class Araba
+//{ 
+
+//    public string Marka { get; set; }
+
+//    public string Model { get;set; }
+
+//    public double KM { get; set; }  
+
+//    public bool Vites { get; set; }
+
+
+//    public override string ToString()
+//    {
+//        Console.WriteLine($"{Marka} marka araba {Model} modelinde {KM} kilometrede {Vites} vitesolarak üretilmiştir.");
+
+//        return base.ToString(); 
+//    }
+
+
+//}
+#endregion
